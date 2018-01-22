b0a1ee8316f1bba6f6e6c3b5d6d691fd2a7f999f
MissakaI <m.iddamalgoda@gmail.com>
2018-01-22 10:39:21 +0530

    Directory Watcher Complete. Untested

--- src/lk/ijse/supportframework/services/DirectoryWatcher.java
+++ src/lk/ijse/supportframework/services/DirectoryWatcher.java
@@ -1,7 +1,10 @@
 package lk.ijse.supportframework.services;
 
+import javafx.collections.ObservableSet;
+
 import java.io.IOException;
 import java.nio.file.InvalidPathException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public interface DirectoryWatcher {
@@ -11,26 +14,28 @@ public interface DirectoryWatcher {
      *
      * @param path Path of the directory to be watched
      * @throws NullPointerException If the pathname argument is null
-     * @throws InvalidPathException If the path declared by pathname does not exist
+     * @throws NoSuchFileException If the file declared by <code>Path</code> does not exist
      * @throws IOException If any other I/O Exception is caused
      */
-    void registerDirectory(Path path) throws NullPointerException, InvalidPathException, IOException;
+    void registerDirectory(Path path) throws NullPointerException, NoSuchFileException, IOException;
 
     /**
      * Adds an directory to the set of watched directories for this object, provided that it is not the same as some directory already in the set.
      * Specified directory will be monitored for all possible <code>WatchEvents</code>
      *
      * @param pathname Absolute or Relative path string to the directory to be watched
-     * @throws NullPointerException If the pathname argument is null
-     * @throws InvalidPathException If the path declared by pathname does not exist
+     * @throws InvalidPathException If the path declared by pathname isn't valid
+     * @throws NoSuchFileException If the file declared by <code>Path</code> does not exist
+     * @throws IOException If any other I/O Exception is caused
      */
-    void registerDirectory(String pathname) throws NullPointerException, InvalidPathException;
+    void registerDirectory(String pathname) throws InvalidPathException, NoSuchFileException, IOException;
 
     /**
      * Removes the directory designated by the path.
      *
      * @param path Path of the directory to be removed.
      * @return true if the watcher contained the specified element
+     * @throws NullPointerException If the <code>Path</code> is null
      */
     boolean removeDirectory(Path path) throws NullPointerException;
 
@@ -39,7 +44,42 @@ public interface DirectoryWatcher {
      *
      * @param pathname Absolute or relative path string to the directory to be watched.
      * @return <code>Path</code> of the removed directory.
-     * @throws NullPointerException If the pathname argument is null.
      */
-    Path removeDirectory(String pathname) throws NullPointerException;
+    Path removeDirectory(String pathname);
+
+    /**
+     * Provides a <code>ObservableSet</code> with the directories of files which were newly created.
+     * <p>
+     * The <code>ObservableSet</code> returned should be provided with a valid <code>ListChangeListener</code>
+     *
+     * @return JavaFX <code>ObservableSet</code> with no valid Listener
+     */
+    ObservableSet<Path> getCreatedSet();
+
+    /**
+     * Provides a <code>ObservableSet</code> with the directories of files which were deleted.
+     * <p>
+     * The <code>ObservableSet</code> returned should be provided with a valid <code>ListChangeListener</code>
+     *
+     * @return JavaFX <code>ObservableSet</code> with no valid Listener
+     */
+    ObservableSet<Path> getDeletedSet();
+
+    /**
+     * Provides a <code>ObservableSet</code> with the directories of files which were modified.
+     * <p>
+     * The <code>ObservableSet</code> returned should be provided with a valid <code>ListChangeListener</code>
+     *
+     * @return JavaFX <code>ObservableSet</code> with no valid Listener
+     */
+    ObservableSet<Path> getModifiedSet();
+
+    /**
+     * Provides a <code>ObservableSet</code> with the directories of files with any type of change.
+     * <p>
+     * The <code>ObservableSet</code> returned should be provided with a valid <code>ListChangeListener</code>
+     *
+     * @return JavaFX <code>ObservableSet</code> with no valid Listener
+     */
+    ObservableSet<Path> getGeneralSet();
 }
--- src/lk/ijse/supportframework/services/impl/DirectoryWatcherImpl.java
+++ src/lk/ijse/supportframework/services/impl/DirectoryWatcherImpl.java
@@ -1,110 +1,150 @@
 package lk.ijse.supportframework.services.impl;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 import lk.ijse.supportframework.services.DirectoryWatcher;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
 public class DirectoryWatcherImpl implements DirectoryWatcher {
     WatchService watcher;
+    Map<Path, WatchKey> watchList = new Hashtable<>();
+    private ObservableSet<Path> createdSet = FXCollections.synchronizedObservableSet(FXCollections.observableSet(new HashSet<Path>()));
+    private ObservableSet<Path> deletedSet = FXCollections.synchronizedObservableSet(FXCollections.observableSet(new HashSet<Path>()));
+    private ObservableSet<Path> modifiedSet = FXCollections.synchronizedObservableSet(FXCollections.observableSet(new HashSet<Path>()));
+    private ObservableSet<Path> generalSet = FXCollections.synchronizedObservableSet(FXCollections.observableSet(new HashSet<Path>()));
+
 
     public DirectoryWatcherImpl() {
         try {
-            watcher= FileSystems.getDefault().newWatchService();
+            watcher = FileSystems.getDefault().newWatchService();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        new Thread(){
-            @Override
-            public void run(){
-                while (true){
-                    WatchKey key = null;
-                    try {
-                        key=watcher.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        new Thread(() -> {
+            while (true) {
+                WatchKey key = null;
+                try {
+                    key = watcher.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Path path = (Path) key.watchable();
+                path = path.toAbsolutePath();
+
+
+                for (WatchEvent<?> watchEvent : key.pollEvents()) {
+                    WatchEvent.Kind<?> kind = watchEvent.kind();
+
+                    WatchEvent<Path> event = (WatchEvent<Path>) watchEvent;
+
+                    if (kind == OVERFLOW) {
+                        continue;
+                    } else if (kind == ENTRY_CREATE) {
+                        createdSet.add(event.context());
+                    } else if (kind == ENTRY_DELETE) {
+                        deletedSet.add(event.context());
+                    } else if (kind == ENTRY_MODIFY) {
+                        modifiedSet.add(event.context());
                     }
-
-                    Path path= (Path) key.watchable();
-                    path=path.toAbsolutePath();
-
-
-                    for (WatchEvent<?> watchEvent : key.pollEvents()) {
-                        WatchEvent.Kind kind=watchEvent.kind();
-
-                        if (kind==OVERFLOW){
-                            continue;
-                        }else if (kind==ENTRY_CREATE){
-
-                        }else if (kind==ENTRY_DELETE){
-
-                        }else if (kind==ENTRY_MODIFY){
-
-                        }
-                    }
-
-                    
-
-                    boolean valid=key.reset();
-                    if (!valid) {
-                        break;
-                    }
+                    generalSet.add(event.context());
 
                 }
+
+
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
+
             }
-        }.start();
+
+        }).start();
     }
 
-    /**
-     * Adds an directory to the set of watched directories for this object, provided that it is not the same as some directory already in the set.
-     * Specified directory will be monitored for all possible <code>WatchEvents</code>
-     *
-     * @param path Path of the directory to be watched
-     * @throws NullPointerException If the pathname argument is null
-     * @throws InvalidPathException If the path declared by pathname does not exist
-     * @throws IOException If any other I/O Exception is caused
-     */
+
     @Override
-    public void registerDirectory(Path path) throws NullPointerException, InvalidPathException, IOException {
-        WatchKey key=path.register(watcher,ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY);
+    public void registerDirectory(Path path) throws NullPointerException, NoSuchFileException, IOException {
+        if (path == null) {
+            throw new NullPointerException();
+        } else {
+            path = path.toAbsolutePath();
+            if (!path.toFile().exists()) {
+                throw new NoSuchFileException(path.toFile().toString());
+            } else {
+                WatchKey key = path.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+                watchList.put(path,key);
+            }
+        }
     }
 
-    /**
-     * Adds an directory to the set of watched directories for this object, provided that it is not the same as some directory already in the set.
-     * Specified directory will be monitored for all possible <code>WatchEvents</code>
-     *
-     * @param pathname Absolute or Relative path string to the directory to be watched
-     * @throws NullPointerException If the pathname argument is null
-     * @throws InvalidPathException If the path declared by pathname does not exist
-     */
     @Override
-    public void registerDirectory(String pathname) throws NullPointerException, InvalidPathException {
-
+    public void registerDirectory(String pathname) throws InvalidPathException, NoSuchFileException, IOException {
+        registerDirectory(Paths.get(pathname));
     }
 
-    /**
-     * Removes the directory designated by the path.
-     *
-     * @param path Path of the directory to be removed.
-     * @return true if the watcher contained the specified element
-     */
     @Override
     public boolean removeDirectory(Path path) throws NullPointerException {
-        return false;
+        if (path == null) {
+            throw new NullPointerException();
+        }
+        path = path.toAbsolutePath();
+        if (watchList.containsKey(path)) {
+            watchList.get(path).cancel();
+            watchList.remove(path);
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    /**
-     * Retrieves and removes the directory designated by the pathname, or null if none are registered or no such path found.
-     *
-     * @param pathname Absolute or relative path string to the directory to be watched.
-     * @return <code>Path</code> of the removed directory.
-     * @throws NullPointerException If the pathname argument is null.
-     */
     @Override
-    public Path removeDirectory(String pathname) throws NullPointerException {
-        return null;
+    public Path removeDirectory(String pathname) {
+        Path path=Paths.get(pathname);
+        path=path.toAbsolutePath();
+        if(removeDirectory(path)){
+            return path;
+        }else
+            return null;
+    }
+
+    public ObservableSet<Path> getCreatedSet() {
+        return createdSet;
+    }
+
+    public void setCreatedSet(ObservableSet<Path> createdSet) {
+        this.createdSet = createdSet;
+    }
+
+    public ObservableSet<Path> getDeletedSet() {
+        return deletedSet;
+    }
+
+    public void setDeletedSet(ObservableSet<Path> deletedSet) {
+        this.deletedSet = deletedSet;
+    }
+
+    public ObservableSet<Path> getModifiedSet() {
+        return modifiedSet;
+    }
+
+    public void setModifiedSet(ObservableSet<Path> modifiedSet) {
+        this.modifiedSet = modifiedSet;
+    }
+
+    public ObservableSet<Path> getGeneralSet() {
+        return generalSet;
+    }
+
+    public void setGeneralSet(ObservableSet<Path> generalSet) {
+        this.generalSet = generalSet;
     }
 }
